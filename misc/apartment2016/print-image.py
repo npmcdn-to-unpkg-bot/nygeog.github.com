2a947209c1a94ab2bf076cc704fadee965250c84
Danny S <dsheehan@cartodb.com>
2016-07-16 20:46:44 -0400

    update

---
+++ misc/apartment2016/print-image.py
@@ -0,0 +1,9 @@
+imgList = ['IMG_2799.jpg','IMG_2818.jpg','IMG_2839.jpg','IMG_2858.jpg','IMG_2877.jpg','IMG_2800.jpg','IMG_2819.jpg','IMG_2840.jpg','IMG_2859.jpg','IMG_2878.jpg','IMG_2801.jpg','IMG_2820.jpg','IMG_2841.jpg','IMG_2860.jpg','IMG_2879.jpg','IMG_2802.jpg','IMG_2821.jpg','IMG_2842.jpg','IMG_2861.jpg','IMG_2880.jpg','IMG_2803.jpg','IMG_2822.jpg','IMG_2843.jpg','IMG_2862.jpg','IMG_2881.jpg','IMG_2804.jpg','IMG_2823.jpg','IMG_2844.jpg','IMG_2863.jpg','IMG_2882.jpg','IMG_2805.jpg','IMG_2824.jpg','IMG_2845.jpg','IMG_2864.jpg','IMG_2883.jpg','IMG_2806.jpg','IMG_2825.jpg','IMG_2846.jpg','IMG_2865.jpg','IMG_2884.jpg','IMG_2807.jpg','IMG_2826.jpg','IMG_2847.jpg','IMG_2866.jpg','IMG_2885.jpg','IMG_2808.jpg','IMG_2827.jpg','IMG_2848.jpg','IMG_2867.jpg','IMG_2886.jpg','IMG_2809.jpg','IMG_2828.jpg','IMG_2849.jpg','IMG_2868.jpg','IMG_2887.jpg','IMG_2810.jpg','IMG_2829.jpg','IMG_2850.jpg','IMG_2869.jpg','IMG_2888.jpg','IMG_2811.jpg','IMG_2830.jpg','IMG_2851.jpg','IMG_2870.jpg','IMG_2889.jpg','IMG_2812.jpg','IMG_2831.jpg','IMG_2852.jpg','IMG_2871.jpg','IMG_2890.jpg','IMG_2813.jpg','IMG_2834.jpg','IMG_2853.jpg','IMG_2872.jpg','IMG_2891.jpg','IMG_2814.jpg','IMG_2835.jpg','IMG_2854.jpg','IMG_2873.jpg','IMG_2892.jpg','IMG_2815.jpg','IMG_2836.jpg','IMG_2855.jpg','IMG_2874.jpg','IMG_2893.jpg','IMG_2816.jpg','IMG_2837.jpg','IMG_2856.jpg','IMG_2875.jpg','IMG_2894.jpg','IMG_2817.jpg','IMG_2838.jpg','IMG_2857.jpg','IMG_2876.jpg','IMG_2895.jpg']
+
+imgList.sort()
+
+for i in imgList:
+	print '!['+i+'](img/'+i+')'
+	print ''
+	print ''
+	print ''
